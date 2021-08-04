a0cd1d3bd9f62e789d31cb0f1594daab17a2714d
sogol-tech <afsharsogol@icloud.com>
2021-08-04 11:57:52 +0100

    Replaced all "class" by "className"

--- src/App.js
+++ src/App.js
@@ -3,80 +3,87 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div class="weather-app">
-        <div class="weather-app-wrapper">
-          <div class="row">
-            <div class="col-4">
-              <div class="current-time">
+      <div className="weather-app">
+        <div className="weather-app-wrapper">
+          <div className="row">
+            <div className="col-4">
+              <div className="current-time">
                 <small id="dt"></small>
               </div>
             </div>
-            <div class="col-4">
-              <span class="float-right">
-                <form class="search" id="searchCity">
+            <div className="col-4">
+              <span className="float-right">
+                <form className="search" id="searchCity">
                   <input
                     type="search"
                     placeholder="Search city..."
-                    class="form-control-sm mb-3"
+                    className="form-control-sm mb-3"
                     id="city-input"
                     autocomplete="off"
                   />
                 </form>
               </span>
             </div>
-            <div class="col-4">
-              <button type="button" class="button" id="currentLocationButton">
-                <i class="fas fa-map-marker-alt"></i>
+            <div className="col-4">
+              <button
+                type="button"
+                className="button"
+                id="currentLocationButton"
+              >
+                <i className="fas fa-map-marker-alt"></i>
               </button>
             </div>
           </div>
 
-          <div class="image-container position-relative">
-            <div class="position-absolute temperature-container">
-              <strong class="temperature font-colour" id="temperature"></strong>
+          <div className="image-container position-relative">
+            <div className="position-absolute temperature-container">
+              <strong
+                className="temperature font-colour"
+                id="temperature"
+              ></strong>
             </div>
             <img
               src="src/images/DAY/1.Clear-sky.jpg"
-              class="rounded mx-auto d-block"
+              className="rounded mx-auto d-block"
               id="img"
               alt="snowy"
               width="400"
             />
           </div>
 
-          <div class="row">
-            <div class="text-end">
-              <small class="units">
-                <a href="#" id="celsius" class="active">
+          <div className="row">
+            <div className="text-end">
+              <small className="units">
+                <a href="#" id="celsius" className="active">
                   C
                 </a>{" "}
                 |
-                <a href="#" class="far" id="fahrenheit">
+                <a href="#" className="far" id="fahrenheit">
                   F
                 </a>
               </small>
             </div>
           </div>
 
-          <h1 class="city" id="city"></h1>
-          <p class="des" id="description"></p>
+          <h1 className="city" id="city"></h1>
+          <p className="des" id="description"></p>
 
-          <div class="row weather-info">
-            <div class="col-6">
+          <div className="row weather-info">
+            <div className="col-6">
               Min: <span id="minTemp"></span>°C
             </div>
-            <div class="col-6">
+            <div className="col-6">
               Max: <span id="maxTemp"></span>°C
             </div>
-            <div class="col-6">
+            <div className="col-6">
               Humidity: <span id="humidity"></span>%
             </div>
-            <div class="col-6">
+            <div className="col-6">
               Wind: <span id="wind"></span> km/h
             </div>
           </div>
           <hr />
-          <div class="weather-forecast" id="forecast"></div>
+          <div className="weather-forecast" id="forecast"></div>
         </div>
       </div>
     </div>
