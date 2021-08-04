bd7c7ade289e138f9718672d5910ff71a54eb500
sogol-tech <afsharsogol@icloud.com>
2021-08-04 11:49:33 +0100

    Imported all CSS code.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>React Weather App</h1>
-        <p>I will be importing weather app made in Vanilla JS into React.</p>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
